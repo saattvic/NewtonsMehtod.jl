db832442b3aae6b078836dc8e34706c5a8b4b302
Saattvic <saattvic@sauder.ubc.ca>
2022-02-15 16:42:39 -0800

    Added functions and tests

--- src/NewtonsMethod.jl
+++ src/NewtonsMethod.jl
@@ -1,5 +1,39 @@
 module NewtonsMethod
 
-# Write your package code here.
+using ForwardDiff, LinearAlgebra
+
+export newtonroot
+
+function iterativesolver(f, x₀, checkconvergence, updatex)
+    xold = x₀.*10
+    fold = 10*f(x₀)
+    xnew = x₀
+    fnew = f(xnew)
+    iter = 1
+    while !checkconvergence(xold, xnew, fold, fnew, iter)
+        xold = xnew
+        xnew = updatex(f, xold)
+        fold = fnew
+        fnew = f(xnew)
+        iter += 1
+    end
+    return(value = xnew, iter=iter)
+end
+
+function newtonroot(f, f′; x₀, tol=1e-7, maxiter=1000)
+    res = iterativesolver(f,
+                          x₀,
+                          (xold, xnew, fold, fnew, iter) -> norm(xnew - xold) < tol || iter > maxiter,
+                          (f, x) -> x - f(x) / f′(x))
+    if res.iter <= maxiter
+        return res
+    else
+        return (value = nothing, iter=res.iter)
+    end
+end
+
+function newtonroot(f; x₀, tol=1e-7, maxiter=1000)
+    return newtonroot(f, x -> ForwardDiff.derivative(f, x); x₀, tol, maxiter)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,49 @@ using NewtonsMethod
 using Test
 
 @testset "NewtonsMethod.jl" begin
-    # Write your tests here.
+    atol = 1e-4
+    f1(x) = 2x+1
+    g1(x) = 2
+    f2(x) = (x-1)^2
+    g2(x) = 2(x-1)
+    f3(x) = exp(x)-1
+    g3(x) = exp(x)
+    f4(x) = 2 + x^2
+    g4(x) = 2x
+
+    # Tests for known functions - analytical derivatives
+    @test isapprox(newtonroot(f1, g1; x₀ = 10.0).value, -0.5, atol=atol)
+    @test isapprox(newtonroot(f2, g2; x₀ = 10.0).value, 1.0, atol=atol)
+    @test isapprox(newtonroot(f3, g3; x₀ = 10.0).value, 0.0, atol=atol)
+
+    # Tests for known functions - automatic derivatives
+    @test isapprox(newtonroot(f1; x₀ = 10.0).value, -0.5, atol=atol)
+    @test isapprox(newtonroot(f2; x₀ = 10.0).value, 1.0, atol=atol)
+    @test isapprox(newtonroot(f3; x₀ = 10.0).value, 0.0, atol=atol)
+
+    # Tests using BigFloat
+    @test isapprox(newtonroot(f1, g1; x₀ = BigFloat(10.0)).value, -0.5, atol=atol)
+    @test isapprox(newtonroot(f2, g2; x₀ = BigFloat(10.0)).value, 1.0, atol=atol)
+    @test isapprox(newtonroot(f3, g3; x₀ = BigFloat(10.0)).value, 0.0, atol=atol)
+    @test isapprox(newtonroot(f1; x₀ = BigFloat(10.0)).value, -0.5, atol=atol)
+    @test isapprox(newtonroot(f2; x₀ = BigFloat(10.0)).value, 1.0, atol=atol)
+    @test isapprox(newtonroot(f3; x₀ = BigFloat(10.0)).value, 0.0, atol=atol)
+
+    # Tests for non-convergence handling
+    @test isnothing(newtonroot(f4, g4; x₀ = 10.0).value)
+    @test isnothing(newtonroot(f4; x₀ = 10.0).value)
+
+    # Tests that maxiter is working
+    @test newtonroot(f4, g4; x₀ = 10.0, maxiter = 5).iter == 6
+    @test newtonroot(f4; x₀ = 10.0, maxiter = 5).iter == 6
+
+    # Tests that tol is working
+    @test abs(newtonroot(f2, g2; x₀ = 10.0, tol = 1e-1).value + 0.5) > 
+          abs(newtonroot(f2, g2; x₀ = 10.0, tol = 1e-10).value + 0.5)
+    @test abs(newtonroot(f2; x₀ = 10.0, tol = 1e-1).value + 0.5) >
+          abs(newtonroot(f2; x₀ = 10.0, tol = 1e-10).value + 0.5)
+    @test abs(newtonroot(f3, g3; x₀ = 10.0, tol = 1e-1).value + 0.5) > 
+          abs(newtonroot(f3, g3; x₀ = 10.0, tol = 1e-10).value + 0.5)
+    @test abs(newtonroot(f3; x₀ = 10.0, tol = 1e-1).value + 0.5) >
+          abs(newtonroot(f3; x₀ = 10.0, tol = 1e-10).value + 0.5)
 end
